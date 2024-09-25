4aaa65d6a0544b0dd4db089cfb697685a737d255
emje-dania <emje7@student.eadania.dk>
2024-09-25 09:52:21 +0200

    intro + the rules

--- Opg.BFC/Opg.BFC/Program.cs
+++ Opg.BFC/Opg.BFC/Program.cs
@@ -10,6 +10,20 @@ namespace Opg.BFC
     {
         static void Main(string[] args)
         {
+            //Varible list
+            bool play = true;
+
+
+            // intro
+            Console.WriteLine("Hello player");
+            Console.WriteLine("Rules: pick between 1-3 in order to play against the computer");
+            Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
