0a1e866e2008446d111074c2bea5a54eac3b8bb4
emje-dania <emje7@student.eadania.dk>
2024-09-26 09:49:15 +0200

    the cpu's choice has been randomized

--- Opg.BFC/Opg.BFC/Program.cs
+++ Opg.BFC/Opg.BFC/Program.cs
@@ -8,22 +8,42 @@ namespace Opg.BFC
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             //Varible list
             bool play = true;
-
+            int min = 1;
+            int max = 3;
+            int myChoice;
+            int rnd;
+            string response;
 
             // intro
             Console.WriteLine("Hello player");
             Console.WriteLine("Rules: pick between 1-3 in order to play against the computer");
             Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
 
+            //While play is true the game will run
+            while (play)
+            {
+                Random randomNumber = new Random();
+                rnd = randomNumber.Next(min, max + 1);
 
+                myChoice = Convert.ToInt32(Console.ReadLine());
 
+                
+
+                }
 
             Console.ReadLine();
 
         }
+
+
+
+
+
     }
+    
 }
