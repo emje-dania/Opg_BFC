40d831bf52f1f5e304d7040ef3b78edceb1356cc
emje-dania <emje7@student.eadania.dk>
2024-09-27 09:52:18 +0200

    game works
    done

--- Opg.BFC/Opg.BFC/Program.cs
+++ Opg.BFC/Opg.BFC/Program.cs
@@ -22,20 +22,84 @@ namespace Opg.BFC
             // intro
             Console.WriteLine("Hello player");
             Console.WriteLine("Rules: pick between 1-3 in order to play against the computer");
-            Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
+            
 
             //While play is true the game will run
             while (play)
             {
+                Console.WriteLine("1. Rock, 2. Paper, 3. Scissors");
+
                 Random randomNumber = new Random();
                 rnd = randomNumber.Next(min, max + 1);
 
                 myChoice = Convert.ToInt32(Console.ReadLine());
 
-                
-
+                switch (myChoice)
+                {
+                    case 1:
+                        {
+                            if (rnd > myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Lost");
+                            }
+                            else if (rnd < myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Won");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nIt was a tie");
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            if(rnd > myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Lost");
+                            }
+                            else if (rnd < myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Won");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nIt was a tie");
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            if(rnd > myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Lost");
+                            }
+                            else if (rnd < myChoice)
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nYou Won");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"You: {myChoice} vs CPU: {rnd}\nIt was a tie");
+                            }
+                            break;
+                        }
                 }
 
+                Console.WriteLine("Play again? (Y/N)");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+                if (response == "Y")
+                {
+                    play = true;
+                }
+                else
+                {
+                    play = false;
+                }
+
+            }
+            Console.WriteLine("Thanks for playing");
             Console.ReadLine();
 
         }
